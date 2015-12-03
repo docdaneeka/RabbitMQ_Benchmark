a3562983a24cdd85eade583c181e0a06006ed579
alex <a.kocieniewski@gmail.com>
2015-12-03 22:17:23 +0100

    change to threads

---
+++ sender/src/main/java/Main.java
@@ -0,0 +1,55 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public final static String QUEUE_NAME = "hello";
+    public static int count = 100000;
+    private static final int threads = 2;
+    private static ArrayList<Sender> threadList;
+
+    public static void main(String[] args) {
+
+        String message = createDataSize(24);
+        threadList = new ArrayList<Sender>();
+
+        for(int i=0; i<threads; i++){
+            Sender sender = new Sender(i, message);
+            threadList.add(sender);
+        }
+
+        for(Sender sender : threadList){
+            sender.start();
+        }
+    }
+
+    private static String createDataSize(int msgSize) {
+        msgSize = msgSize/2;
+        msgSize = msgSize * 1024;
+        StringBuilder sb = new StringBuilder(msgSize);
+        for (int i=0; i<msgSize; i++) {
+            sb.append('a');
+        }
+        return sb.toString();
+    }
+
+    public static void writeReport(String report){
+        try {
+            File file = new File("sender_report.txt");
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(report + "\n");
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- sender/src/main/java/Sender.java
+++ sender/src/main/java/Sender.java
@@ -1,63 +1,61 @@
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class Sender {
+/**
+ * Created by alex on 03.12.15.
+ */
+public class Sender extends Thread{
 
-    private final static String QUEUE_NAME = "hello";
-    static long startTime;
-    private static int count = 0;
-    public static void main(String[] args) throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
+    private ConnectionFactory factory;
+    private Connection connection;
+    private Channel channel;
+
+    private long startTime;
+    private String message;
+    private int no;
+
+    public Sender(int no, String message){
+        this.no = no;
+        this.message = message;
+        factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        count = 100000;
-        String message = createDataSize(24);
-        startTime = System.nanoTime();
-        for(int i=0;i<count;i++) {
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        }
-        String report = count + " messages sent after " + ((System.nanoTime() - startTime)/1000000000.0) + "s";
-        System.out.println(report);
-        writeReport(report);
-
-        channel.close();
-        connection.close();
-    }
-
-    private static String createDataSize(int msgSize) {
-        msgSize = msgSize/2;
-        msgSize = msgSize * 1024;
-        StringBuilder sb = new StringBuilder(msgSize);
-        for (int i=0; i<msgSize; i++) {
-            sb.append('a');
-        }
-        return sb.toString();
-    }
-
-    private static void writeReport(String report){
         try {
-            File file = new File("sender_report.txt");
-
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(report + "\n");
-            bw.close();
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.queueDeclare(Main.QUEUE_NAME, false, false, false, null);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
         }
+    }
+
+    @Override
+    public void run() {
+        startTime = System.nanoTime();
+        for(int i=0;i<Main.count;i++) {
+            try {
+                channel.basicPublish("", Main.QUEUE_NAME, null, message.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            channel.close();
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        String report = "Thread no" + no + " " + Main.count + " messages sent after " + ((System.nanoTime() - startTime)/1000000000.0) + "s";
+        System.out.println(report);
+        Main.writeReport(report);
     }
 }
